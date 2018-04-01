d498e02f0312379c71f474cdb027b080c60e4828
Marco Borromeo <marco@marco-borromeo.com>
2018-04-02 00:58:57 +0200

    Adding chi support

---
+++ chi/chi_suite_test.go
@@ -0,0 +1,13 @@
+package chiadapter_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestChi(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Chi Suite")
+}
