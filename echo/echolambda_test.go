7cc31155885e01a1d16fb74ab574cc91f72f9dee
watermelon <80680489@qq.com>
2019-05-09 00:13:22 +0800

    Add echo support (#36)
    * support for [echo framework](https://github.com/labstack/echo)

---
+++ echo/echolambda_test.go
@@ -0,0 +1,37 @@
+package echoadapter_test
+
+import (
+	"log"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/awslabs/aws-lambda-go-api-proxy/echo"
+	"github.com/labstack/echo"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("EchoLambda tests", func() {
+	Context("Simple ping request", func() {
+		It("Proxies the event correctly", func() {
+			log.Println("Starting test")
+			e := echo.New()
+			e.GET("/ping", func(c echo.Context) error {
+				log.Println("Handler!!")
+				return c.String(200, "pong")
+			})
+
+			adapter := echoadapter.New(e)
+
+			req := events.APIGatewayProxyRequest{
+				Path:       "/ping",
+				HTTPMethod: "GET",
+			}
+
+			resp, err := adapter.Proxy(req)
+
+			Expect(err).To(BeNil())
+			Expect(resp.StatusCode).To(Equal(200))
+		})
+	})
+})
