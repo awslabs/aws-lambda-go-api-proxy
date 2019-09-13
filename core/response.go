5e29586ae8cbf276fdc87b5f9ce5b08d39205ffd
Aldrin Leal <aldrin@leal.eng.br>
2019-09-12 19:45:19 -0500

    Removing defer

--- core/response.go
+++ core/response.go
@@ -85,7 +85,7 @@ func (r *ProxyResponseWriter) WriteHeader(status int) {
 // Returns a populated proxy response object. If the response is invalid, for example
 // has no headers or an invalid status code returns an error.
 func (r *ProxyResponseWriter) GetProxyResponse() (events.APIGatewayProxyResponse, error) {
-	defer r.notifyClosed()
+	r.notifyClosed()
 
 	if r.status == defaultStatusCode {
 		return events.APIGatewayProxyResponse{}, errors.New("Status code not set on response")
