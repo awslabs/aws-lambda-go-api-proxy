a0b7b0ab218bce9b79713149993d047525433638
Jim Ancona <jim@anconafamily.com>
2020-09-29 16:10:32 -0400

    To avoid duplicate response headers, only populate MultiValueHeaders (#73)

--- core/response.go
+++ core/response.go
@@ -103,15 +103,8 @@ func (r *ProxyResponseWriter) GetProxyResponse() (events.APIGatewayProxyResponse
 		isBase64 = true
 	}
 
-	proxyHeaders := make(map[string]string)
-
-	for h := range r.headers {
-		proxyHeaders[h] = r.headers.Get(h)
-	}
-
 	return events.APIGatewayProxyResponse{
 		StatusCode:        r.status,
-		Headers:           proxyHeaders,
 		MultiValueHeaders: http.Header(r.headers),
 		Body:              output,
 		IsBase64Encoded:   isBase64,
