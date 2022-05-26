404beb10212780e9a5644bc762b092313b202d73
Bhavik Shah <bhavik1st@users.noreply.github.com>
2022-05-26 20:34:29 +0530

    Updated example to remove local execution context

--- examples/fiber/main.go
+++ examples/fiber/main.go
@@ -11,24 +11,19 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-var runOnAwsLambda = true
 var fiberLambda *fiberadapter.FiberLambda
-var app *fiber.App
 
 // init the Fiber Server
 func init() {
 	log.Printf("Fiber cold start")
-
+	var app *fiber.App
 	app = fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
 	})
 
-	if runOnAwsLambda {
-		log.Printf("Fiber Addapter New")
-		fiberLambda = fiberadapter.New(app)
-	}
+	fiberLambda = fiberadapter.New(app)
 }
 
 // Handler will deal with Fiber working with Lambda
@@ -38,13 +33,6 @@ func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.API
 }
 
 func main() {
-
-	if runOnAwsLambda {
-		// Make the handler available for Remote Procedure Call by AWS Lambda
-		log.Printf("Fiber Adapter Lambda Handler")
-		lambda.Start(Handler)
-	} else {
-		log.Printf("Fiber Listen Handler")
-		log.Fatal(app.Listen(":3000"))
-	}
+	// Make the handler available for Remote Procedure Call by AWS Lambda
+	lambda.Start(Handler)
 }
