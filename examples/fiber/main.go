3cc64100cdb76288e8d8550e5971e9585b59e7c6
Bhavik Shah <bhavik1st@users.noreply.github.com>
2022-05-26 20:28:42 +0530

    Created example for fiber adapter

---
+++ examples/fiber/main.go
@@ -0,0 +1,50 @@
+// main.go
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	fiberadapter "github.com/awslabs/aws-lambda-go-api-proxy/fiber"
+	"github.com/gofiber/fiber/v2"
+)
+
+var runOnAwsLambda = false
+var fiberLambda *fiberadapter.FiberLambda
+var app *fiber.App
+
+// init the Fiber Server
+func init() {
+	log.Printf("Fiber cold start")
+
+	app = fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	if runOnAwsLambda {
+		log.Printf("Fiber Addapter New")
+		fiberLambda = fiberadapter.New(app)
+	}
+}
+
+// Handler will deal with Fiber working with Lambda
+func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	// If no name is provided in the HTTP request body, throw an error
+	return fiberLambda.ProxyWithContext(ctx, req)
+}
+
+func main() {
+
+	if runOnAwsLambda {
+		// Make the handler available for Remote Procedure Call by AWS Lambda
+		log.Printf("Fiber Adapter Lambda Handler")
+		lambda.Start(Handler)
+	} else {
+		log.Printf("Fiber Listen Handler")
+		log.Fatal(app.Listen(":3000"))
+	}
+}
